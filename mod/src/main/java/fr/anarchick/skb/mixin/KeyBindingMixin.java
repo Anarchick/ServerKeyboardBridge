4359063fcf7fe82f978d95d43f31b98da09d3492
aeim <aeim@hotmail.fr>
2024-10-21 12:34:54 +0200

    fix KeyBindingMixin sending key in any screen since mc1.20.4

--- mod/src/main/java/fr/anarchick/skb/mixin/KeyBindingMixin.java
+++ mod/src/main/java/fr/anarchick/skb/mixin/KeyBindingMixin.java
@@ -3,6 +3,7 @@ package fr.anarchick.skb.mixin;
 import fr.anarchick.skb.event.KeyPressEvent;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,9 +15,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(KeyBinding.class)
 public abstract class KeyBindingMixin {
 
+	private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
+
 	@Inject(method = "setKeyPressed", at = @At("HEAD"))
 	private static void input(InputUtil.Key key, boolean pressed, CallbackInfo ci) {
-		KeyPressEvent.onKeyEvent(key.getCode(), pressed);
+		// On mc 1.20.4+ this method is called even if the client is in a screen
+		// This method is called often so we can't use instanceof HandledScreen here
+		// It's better to use GUIScreenMixin instead
+		if (CLIENT.currentScreen == null) {
+			KeyPressEvent.onKeyEvent(key.getCode(), pressed);
+		}
 	}
 
 }
